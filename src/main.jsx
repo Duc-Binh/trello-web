8482b501160ba22cf1ce392e649797b69bc604ac
ducbinh <taducbinh18@gmail.com>
2025-11-30 10:17:59 +0700

    relative-absolute-import

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import CssBaseline from "@mui/material/CssBaseline";
+
+// import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+// import theme from "./theme.js";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <CssVarsProvider theme={theme}>
+//       <CssBaseline />
+//       <App />
+//     </CssVarsProvider>
+//   </React.StrictMode>
+// );
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "~/App.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
-
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
-import theme from "./theme.js";
+import theme from "~/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,16 +28,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <CssBaseline />
       <App />
     </CssVarsProvider>
-
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-
   </React.StrictMode>
 );
